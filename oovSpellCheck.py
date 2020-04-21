8ee4b3b231dbcd583fddeda3aa25ba4a144f6150
R1j1t <22280243+R1j1t@users.noreply.github.com>
2020-04-22 00:47:05 +0530

    black code formatting and vocab upload

--- oovSpellCheck.py
+++ oovSpellCheck.py
@@ -8,23 +8,26 @@ from spacy.vocab import Vocab
 
 from transformers import AutoModelWithLMHead, AutoTokenizer
 
-class oovChecker():
+
+class oovChecker:
     """Class object for Out Of Vocabulary(OOV) corrections 
     """
-    
-    def __init__(self,debug=False):
-        self.nlp = spacy.load("en_core_web_sm", disable=["tagger", "parser"]) # using default tokeniser with NER
-        with open('./uncased_L-4_H-512_A-8/vocab.txt') as f:
+
+    def __init__(self, debug=False):
+        self.nlp = spacy.load(
+            "en_core_web_sm", disable=["tagger", "parser"]
+        )  # using default tokeniser with NER
+        with open("./uncased_L-4_H-512_A-8/vocab.txt") as f:
             # if want to remove '[unusedXX]' from vocab
             # words = [line.rstrip() for line in f if not line.startswith('[unused')]
             words = [line.rstrip() for line in f]
         self.vocab = Vocab(strings=words)
         self.BertTokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
         self.BertModel = AutoModelWithLMHead.from_pretrained("bert-base-cased")
-        self.mask = self.BertTokenizer.mask_token            
+        self.mask = self.BertTokenizer.mask_token
         self.debug = debug
-        
-    def check(self, query=''):
+
+    def check(self, query=""):
         """Complete pipeline which returns update query
 
         Keyword Arguments:
@@ -33,26 +36,25 @@ class oovChecker():
         Returns:
             {str} -- returns updated query with spelling corrections (if any)
         """
-        if type(query) != str and len(query)==0:
-            print('Invalid query, expected non empty `str` but passed',query)
-            
+        if type(query) != str and len(query) == 0:
+            print("Invalid query, expected non empty `str` but passed", query)
+
         misspellTokens, doc = self.misspellIdentify(query)
-        if len(misspellTokens)>0:
-            candidate = self.candidateGenerator(misspellTokens,query=query) 
+        if len(misspellTokens) > 0:
+            candidate = self.candidateGenerator(misspellTokens, query=query)
             answer = self.candidateRanking(candidate)
-            updatedQuery = ''
+            updatedQuery = ""
             for i in doc:
                 if i in misspellTokens:
                     updatedQuery += answer[i] + " "
                 else:
                     updatedQuery += i.text + " "
-                
-            print('Did you mean: ',updatedQuery)
-            print('Original text:', query)
-        return updatedQuery
-  
 
-    def misspellIdentify(self, query=''):
+            print("Did you mean: ", updatedQuery)
+            print("Original text:", query)
+        return updatedQuery
+
+    def misspellIdentify(self, query=""):
         """To identify misspelled words from the query
 
         At present, All the following criteria should be met for word to be misspelled
@@ -68,20 +70,22 @@ class oovChecker():
             {tuple} -- returns `List[`Token`]` and `Doc`
         """
 
-                    
         doc = self.nlp(query)
         misspell = []
         for token in doc:
-            if((token.text.lower() not in self.vocab) and 
-               (token.ent_type_ != 'PERSON') and 
-               (not token.like_num)):
-                
+            if (
+                (token.text.lower() not in self.vocab)
+                and (token.ent_type_ != "PERSON")
+                and (not token.like_num)
+            ):
+
                 misspell.append(token)
-        
-        if self.debug: print(misspell)
+
+        if self.debug:
+            print(misspell)
         return (misspell, doc)
-    
-    def candidateGenerator(self, misspellings, top_n=5 ,query=''):
+
+    def candidateGenerator(self, misspellings, top_n=5, query=""):
         """Returns Candidates for misspells
 
         This function is responsible for generating candidate list for misspell
@@ -102,32 +106,41 @@ class oovChecker():
         """
 
         response = {}
-            
+
         for token in misspellings:
             updatedQuery = query
             updatedQuery = updatedQuery.replace(token.text, self.mask)
-            if self.debug: print("For",'`'+token.text+'`',"updated query is:\n",updatedQuery)
+            if self.debug:
+                print(
+                    "For", "`" + token.text + "`", "updated query is:\n", updatedQuery
+                )
 
             model_input = self.BertTokenizer.encode(updatedQuery, return_tensors="pt")
-            mask_token_index = torch.where(model_input == self.BertTokenizer.mask_token_id)[1]
+            mask_token_index = torch.where(
+                model_input == self.BertTokenizer.mask_token_id
+            )[1]
             token_logits = self.BertModel(model_input)[0]
             mask_token_logits = token_logits[0, mask_token_index, :]
 
-            top_n_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()  
-            if self.debug: print('top_n_tokens:',top_n_tokens)
-            
+            top_n_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
+            if self.debug:
+                print("top_n_tokens:", top_n_tokens)
+
             if token not in response:
-                response[token] = [self.BertTokenizer.decode([candidateWord]) for candidateWord in top_n_tokens]
-                
+                response[token] = [
+                    self.BertTokenizer.decode([candidateWord])
+                    for candidateWord in top_n_tokens
+                ]
+
             # for candidate in top_5_tokens:
-                # response[token].append(self.BertTokenizer.decode([candidate]))
-                # print(updatedQuery.replace(self.mask, self.BertTokenizer.decode([candidate])))
-            
-            if self.debug: print(response)
-                
+            # response[token].append(self.BertTokenizer.decode([candidate]))
+            # print(updatedQuery.replace(self.mask, self.BertTokenizer.decode([candidate])))
+
+            if self.debug:
+                print(response)
 
         return response
-    
+
     def candidateRanking(self, misspellingsDict):
         """Ranking the candidates based on edit Distance
 
@@ -144,24 +157,25 @@ class oovChecker():
         Returns:
             Dict{`Token`:{str}} -- Eg of return type {misspell-1:'BEST-CANDIDATE'}
         """
-                
-        response={}
-#         doc = self.nlp(query)
+
+        response = {}
+        #         doc = self.nlp(query)
         for misspell in misspellingsDict:
             ## Init least_edit distance
             least_edit_dist = 100
-            
-            if self.debug: print('misspellingsDict[misspell]', misspellingsDict[misspell])
+
+            if self.debug:
+                print("misspellingsDict[misspell]", misspellingsDict[misspell])
             for candidate in misspellingsDict[misspell]:
-                edit_dist = editdistance.eval(misspell.text,candidate)
+                edit_dist = editdistance.eval(misspell.text, candidate)
                 if edit_dist < least_edit_dist:
                     least_edit_dist = edit_dist
                     response[misspell] = candidate
-                    
-            if self.debug: print(response)
+
+            if self.debug:
+                print(response)
         return response
-        
-                
+
 
 def timeLog(fnName, relativeTime):
     """For time log
@@ -175,16 +189,15 @@ def timeLog(fnName, relativeTime):
     """
 
     timeNow = datetime.datetime.now()
-    print(fnName, "took: ",timeNow - relativeTime)
+    print(fnName, "took: ", timeNow - relativeTime)
     return datetime.datetime.now()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Code running...")
-    start=datetime.datetime.now()
+    start = datetime.datetime.now()
     checker = oovChecker()
-    modelLoadTime = timeLog("Model Loading",start)
+    modelLoadTime = timeLog("Model Loading", start)
 
     checker.check("Income was $9.4 million compared to the prior year of $2.7 milion.")
-    checkerTime = timeLog('Correction (if any)', modelLoadTime)
-
+    checkerTime = timeLog("Correction (if any)", modelLoadTime)
