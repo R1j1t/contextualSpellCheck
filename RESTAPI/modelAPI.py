598897c7ca5b77034b806c3b4cbdc64b62a18898
R1j1t <22280243+R1j1t@users.noreply.github.com>
2020-05-09 01:40:09 +0530

    black code formatting

--- RESTAPI/modelAPI.py
+++ RESTAPI/modelAPI.py
@@ -1,11 +1,11 @@
 from flask import request
-from contextualSpellCheck.contextualSpellCheck import contextualSpellCheck 
+from contextualSpellCheck.contextualSpellCheck import contextualSpellCheck
 import spacy
 import json
 from flask import Flask, render_template, request, make_response, jsonify
 
 
-#url
+# url
 # http://10.1.1.1:5000/login/alex
 
 app = Flask(__name__)
@@ -13,19 +13,19 @@ app = Flask(__name__)
 nlp = spacy.load("en_core_web_sm", disable=["tagger", "parser"])
 checker = contextualSpellCheck(debug=False)
 nlp.add_pipe(checker)
-        
-    
-@app.route('/', methods=['GET'])
+
+
+@app.route("/", methods=["GET"])
 def model_query():
-    query = request.args.get('query')
+    query = request.args.get("query")
 
     doc = nlp(query)
-    correctionScore={}
+    correctionScore = {}
     duplicate_counter = 1
     for key, value in doc._.score_spellCheck.items():
         if key.text in correctionScore:
-            key = key.text+ ":"+str(duplicate_counter)
-            duplicate_counter +=1
+            key = key.text + ":" + str(duplicate_counter)
+            duplicate_counter += 1
         else:
             key = key.text
             duplicate_counter = 1
@@ -33,14 +33,21 @@ def model_query():
 
     with doc.retokenize() as retokenizer:
         retokenizer.merge(doc[0:])
-    
-    response = make_response(json.dumps({'success': True, 
-                                         "input":doc.text,
-                                         'corrected': doc._.outcome_spellCheck, 
-                                         "suggestion_score":correctionScore}))
+
+    response = make_response(
+        json.dumps(
+            {
+                "success": True,
+                "input": doc.text,
+                "corrected": doc._.outcome_spellCheck,
+                "suggestion_score": correctionScore,
+            }
+        )
+    )
     response.status_code = 200
-    response.headers['Content-Type'] = 'application/json'
+    response.headers["Content-Type"] = "application/json"
     return response
 
+
 if __name__ == "__main__":
-	app.run()
+    app.run()
--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
+    python_requires=">=3.5",
 )
