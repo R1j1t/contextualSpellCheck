705ceeb4443a3b3087074b1330b5bf801053551a
jonmun <jonny.munro@hotmail.co.uk>
2021-02-10 13:48:34 +0000

    Resolved #48 by migrating the code to match the nlp pipeline in spacy v3. See: https://nightly.spacy.io/usage/v3#migrating-add-pipe

--- contextualSpellCheck/contextualSpellCheck.py
+++ contextualSpellCheck/contextualSpellCheck.py
@@ -11,8 +11,10 @@ import torch
 from spacy.tokens import Doc, Token, Span
 from spacy.vocab import Vocab
 from transformers import AutoModelForMaskedLM, AutoTokenizer
+from spacy.language import Language
 
 
+@Language.factory("contextual_spellchecker")
 class ContextualSpellCheck(object):
     """
     Class object for Out Of Vocabulary(OOV) corrections
@@ -22,6 +24,8 @@ class ContextualSpellCheck(object):
 
     def __init__(
         self,
+        nlp,
+        name,
         vocab_path="",
         model_name="bert-base-cased",
         max_edit_dist=10,
