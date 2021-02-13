305410e9019a657c26b97cfc2e0d8b243c68ea38
R1j1t <22280243+R1j1t@users.noreply.github.com>
2021-02-13 18:35:20 +0530

    updated README and controller

--- contextualSpellCheck/__init__.py
+++ contextualSpellCheck/__init__.py
@@ -5,6 +5,4 @@ __all__ = ["ContextualSpellCheck", "add_to_pipe"]
 
 
 def add_to_pipe(nlp, **kwargs):
-    checker = ContextualSpellCheck(**kwargs)
-    nlp.add_pipe(checker)
-    return nlp
+    nlp.add_pipe("contextual spellchecker", **kwargs)
